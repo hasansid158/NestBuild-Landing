6bc4f22add6a6db7396df021fdd5f19adf3d647d
hasansid158 <48666786+hasansid158@users.noreply.github.com>
2019-10-07 05:07:39 +1100

    NestBuilds

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
